9dd7c6a781ec6309eaf4f8756e2c093fb7d15cc2
Matematej <87311529+Matematej@users.noreply.github.com>
2024-08-11 17:29:08 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,18 @@
+variable "rg_name" {
+  description = "Jméno Resource Group"
+  type        = string
+  default     = "profx-prod-rg"
+}
+
+variable "location" {
+  description = "Location of resources"
+  type        = string
+  default     = "westeurope"
+}
+
 variable "api_key" {
 description = "Secret API key"
 type = string
 sensitive = true 
+default = "NotASecurePassword"
 }
